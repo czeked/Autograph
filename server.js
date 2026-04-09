573a7a303e413712d0eb33f105e33927dee0e78a
czeked <prrrst1s@gmail.com>
2026-04-09 10:50:15 +0200

    ...

--- server.js
+++ server.js
@@ -4,17 +4,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// strona główna
 app.get("/", (req, res) => {
   res.send("Dziala Express 🚀");
 });
 
-// przykładowe API
 app.get("/api", (req, res) => {
   res.json({ message: "To jest API" });
 });
 
-// uruchomienie serwera
 app.listen(port, () => {
   console.log(`Serwer działa na http://localhost:${port}`);
 });
