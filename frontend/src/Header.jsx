b86a4d2486b29ca651db3799b5c6ae7a77499903
czeked <prrrst1s@gmail.com>
2026-04-13 15:50:30 +0200

    ostry obraz poprawka FRONTEND

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
         <div className="header">
             <div className="icons">
                 <i className="fa-regular fa-user"></i>
-                <i className="fa-solid fa-meteor"></i>
+                <i className="fa-solid fa-chart-simple"></i>
             </div>
             <h1>Autograph</h1>
         </div>
