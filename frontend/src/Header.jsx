cb6622af5a5e95865c1d5299f0fb8d71102865a7
czeked <prrrst1s@gmail.com>
2026-04-13 12:19:57 +0200

    dodanie homePage FRONTEND

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="icons">
+                <i className="fa-regular fa-user"></i>
+                <i className="fa-solid fa-meteor"></i>
+            </div>
+            <h1>Autograph</h1>
+        </div>
+        
+    )
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AiAnalyzer from './AiAnalyzer.jsx'
-import NewsPage from '../NewsPage.jsx'
-
-
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AiAnalyzer />
-    <NewsPage />
+    <App />
   </StrictMode>,
 )
